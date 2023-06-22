4ee780e061f6d6fd32cb4276d487ddc64f5c5973
SHIN <zxzxvcdd@gmail.com>
2023-06-22 17:24:07 +0900

    213123

---
+++ src/test/com/kgcoffee/web/menu/TestInsertBoard.java
@@ -0,0 +1,52 @@
+package test.com.kgcoffee.web.menu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import com.kgcoffee.web.board.dao.SnsDAO;
+
+class TestInsertBoard {
+
+	@Test
+	void testExecute() {
+
+		try {
+			SnsDAO dao = new SnsDAO();
+
+			String jemok = "";
+			String writer = "유저";
+			String content = "";
+			String filename = "";
+
+			for (int i = 1; i <= 100; i++) {
+				
+				
+				jemok = "제목"+i;
+				content = "내용입니다"+i;
+				filename="";
+				
+				writer = "유저"+i;
+			
+				
+				
+				
+				
+
+			}
+
+			assertEquals(true, dao.insert(jemok, writer, content, filename));
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/test/com/kgcoffee/web/menu/TestInsertUser.java
@@ -0,0 +1,42 @@
+package test.com.kgcoffee.web.menu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import com.kgcoffee.web.users.dao.UsersDAO;
+
+class TestInsertUser {
+
+	@Test
+	void testExecute() {
+
+		try {
+			UsersDAO dao = UsersDAO.getInstance();
+
+			for (int i = 0; i < 100; i++) {
+
+				String user_id = "유저" + i;
+				String user_pw = "유저" + i;
+				String user_name = "유저" + i;
+				
+				String birthday = "1993-01-01";
+				String tel = "010-1234-1234"; 
+
+				assertEquals(true, 
+						dao.insert_user(user_id, user_pw, user_name, birthday, tel));
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
