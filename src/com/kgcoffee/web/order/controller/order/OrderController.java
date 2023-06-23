927ca6309309cafa3c2007fc126fb28803c82bb3
zxzxvcdd <zxzxvcdd@gmail.com>
2023-06-23 18:06:32 +0900

    123

--- src/com/kgcoffee/web/order/controller/order/OrderController.java
+++ src/com/kgcoffee/web/order/controller/order/OrderController.java
@@ -1,14 +1,38 @@
 package com.kgcoffee.web.order.controller.order;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
+import com.kgcoffee.web.kakaoMap.dao.KakaoMapDAO;
+import com.kgcoffee.web.kakaoMap.vo.KakaoMapVO;
 import com.kgcoffee.web.order.Controller;
+import com.kgcoffee.web.order.dao.CartRepository;
+import com.kgcoffee.web.order.domain.CartVO;
 
 public class OrderController implements Controller {
     @Override
     public String process(Map<String, String> paramMap, Map<String, Object> model) {
-        model.put("userId", paramMap.get("userId"));
+       
+    	String userId = paramMap.get("userId");
+   
+        
+        CartRepository cartRepository = new CartRepository();
+        
+        
+        ArrayList<CartVO> cartList = cartRepository.findAllCartsByUserId(userId);
+        ArrayList<KakaoMapVO> storeList =null;
+        HashMap<String, Object> keyMap = new HashMap<String, Object>();
+        
+        
+        storeList = KakaoMapDAO.getInstance().findMap(keyMap);
+        System.out.println(storeList);
+        
+     	model.put("userId", userId);
         model.put("totalPrice", paramMap.get("totalPrice"));
+        model.put("storeList", storeList);
+        model.put("cartList", cartList);
+        
         return "order-form";
     }
 }
